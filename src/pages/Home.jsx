7ff81df8ac1b8330e4b79daa3f9c6232ab19e02b
Umar Ibrahim Ayobami (stopbegging95) <stopbeging@gmail.com>
2022-11-30 12:35:26 +0100

    bulq-ecommerce-project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/topbar/Navbar"
+
+const Home = () => {
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home
---
+++ tempData.js
@@ -0,0 +1,62 @@
+export const productData = [
+    {
+        id: 1,
+        product: "Groceries",
+        picture: "https://images.pexels.com/photos/2733918/pexels-photo-2733918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 2,
+        product: "Home and Office",
+        picture: "https://images.pexels.com/photos/2883049/pexels-photo-2883049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 3,
+        product: "Men Fashion",
+        picture: "https://images.pexels.com/photos/1342609/pexels-photo-1342609.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 4,
+        product: "Women Fashion",
+        picture: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 5,
+        product: "Phone",
+        picture: "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 6,
+        product: "Gaming",
+        picture: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 7,
+        product: "Computing",
+        picture: "https://images.pexels.com/photos/5474285/pexels-photo-5474285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 8,
+        product: "Fitness",
+        picture: "https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 9,
+        product: "Electronics",
+        picture: "https://images.pexels.com/photos/39290/mother-board-electronics-computer-board-39290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 10,
+        product: "Baby Wears",
+        picture: "https://images.pexels.com/photos/249209/pexels-photo-249209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 11,
+        product: "Auto Mobile",
+        picture: "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: 12,
+        product: "Others",
+        picture: "https://images.pexels.com/photos/5088188/pexels-photo-5088188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+]
